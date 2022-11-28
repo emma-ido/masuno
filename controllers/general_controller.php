f61e8f7113927f822b8c0c6f576b25375fead152
Emma-Ido Ukpong <emmankelam@gmail.com>
2022-11-28 12:02:56 +0000

    Adding skeleton template to project

---
+++ controllers/general_controller.php
@@ -0,0 +1,18 @@
+<?php
+//connect to the user account class
+include("../classes/general_class.php");
+
+//sanitize data
+
+
+
+
+//--INSERT--//
+
+//--SELECT--//
+
+//--UPDATE--//
+
+//--DELETE--//
+
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+	// landing/index page
+?>
+
+	
